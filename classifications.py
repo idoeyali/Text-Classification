ad98180fc957e83d54efe048d2d017d6e819e70b
ido_eyali <ido.eyali@mail.huji.ac.il>
2024-04-07 16:22:22 +0300

    upload

---
+++ classifications.py
@@ -0,0 +1,202 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+# subset of categories that we will use
+category_dict = {'comp.graphics': 'computer graphics',
+                 'rec.sport.baseball': 'baseball',
+                 'sci.electronics': 'science, electronics',
+                 'talk.politics.guns': 'politics, guns'
+                 }
+
+
+def get_data(categories=None, portion=1.):
+    """
+    Get data for given categories and portion
+    :param portion: portion of the data to use
+    :return:
+    """
+    # get data
+    from sklearn.datasets import fetch_20newsgroups
+    data_train = fetch_20newsgroups(categories=categories, subset='train', remove=('headers', 'footers', 'quotes'),
+                                    random_state=21)
+    data_test = fetch_20newsgroups(categories=categories, subset='test', remove=('headers', 'footers', 'quotes'),
+                                   random_state=21)
+
+    # train
+    train_len = int(portion * len(data_train.data))
+    x_train = np.array(data_train.data[:train_len])
+    y_train = data_train.target[:train_len]
+    # remove empty entries
+    non_empty = x_train != ""
+    x_train, y_train = x_train[non_empty].tolist(), y_train[non_empty].tolist()
+
+    # test
+    x_test = np.array(data_test.data)
+    y_test = data_test.target
+    non_empty = np.array(x_test) != ""
+    x_test, y_test = x_test[non_empty].tolist(), y_test[non_empty].tolist()
+    return x_train, y_train, x_test, y_test
+
+
+def linear_classification(portion=1.):
+    """
+    Perform linear classification
+    :param portion: portion of the data to use
+    :return: classification accuracy
+    """
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import accuracy_score
+    tf = TfidfVectorizer(stop_words='english', max_features=1000)
+    x_train, y_train, x_test, y_test = get_data(categories=category_dict.keys(), portion=portion)
+
+    # Add your code here
+    x_train_embedding = tf.fit_transform(x_train)
+    x_test_embedding = tf.transform(x_test)
+    model = LogisticRegression()
+
+    # Fit model
+    model.fit(x_train_embedding, y_train)
+    prediction = model.predict(x_test_embedding)
+
+    return accuracy_score(y_test, prediction)
+
+
+def transformer_classification(portion=1.):
+    """
+    Transformer fine-tuning.
+    :param portion: portion of the data to use
+    :return: classification accuracy
+    """
+    import torch
+
+    class Dataset(torch.utils.data.Dataset):
+        """
+        Dataset object
+        """
+
+        def __init__(self, encodings, labels):
+            self.encodings = encodings
+            self.labels = labels
+
+        def __getitem__(self, idx):
+            item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+            item['labels'] = torch.tensor(self.labels[idx])
+            return item
+
+        def __len__(self):
+            return len(self.labels)
+
+    from datasets import load_metric
+    metric = load_metric("accuracy")
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    from transformers import Trainer, TrainingArguments
+    from transformers import AutoModelForSequenceClassification, AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained('distilroberta-base', cache_dir=None)
+    model = AutoModelForSequenceClassification.from_pretrained('distilroberta-base',
+                                                               cache_dir=None,
+                                                               num_labels=len(category_dict),
+                                                               problem_type="single_label_classification")
+
+    x_train, y_train, x_test, y_test = get_data(categories=category_dict.keys(), portion=portion)
+
+    # Use the DataSet object defined above. No need for a DataCollator
+    learning_rate = 5e-5
+    n_epochs = 3
+    batch_size = 16
+    trainingArgs = TrainingArguments(output_dir="/content/drive/MyDrive/NLP-ex4",
+                                     learning_rate=learning_rate,
+                                     num_train_epochs=n_epochs,
+                                     per_device_train_batch_size=batch_size,
+                                     per_device_eval_batch_size=batch_size,
+                                     evaluation_strategy="epoch",
+                                     logging_strategy="epoch")
+
+    # Tokenized our train and test sets.
+    x_train_tokenized = tokenizer.__call__(x_train, padding='longest', truncation=True)
+    x_test_tokenized = tokenizer.__call__(x_test, padding='longest', truncation=True)
+
+    # Creating datasets
+    train_dataset = Dataset(x_train_tokenized, y_train)
+
+    test_dataset = Dataset(x_test_tokenized, y_test)
+
+    # Init our trainer.
+    model_trainer = Trainer(model=model,
+                            args=trainingArgs,
+                            train_dataset=train_dataset,
+                            eval_dataset=test_dataset,
+                            compute_metrics=compute_metrics)
+    model_trainer.train()
+    return model_trainer.evaluate()
+
+
+
+def zeroshot_classification(portion=1.):
+    """
+    Perform zero-shot classification
+    :param portion: portion of the data to use
+    :return: classification accuracy
+    """
+    from transformers import pipeline
+    from sklearn.metrics import accuracy_score
+    x_train, y_train, x_test, y_test = get_data(categories=category_dict.keys(), portion=portion)
+    clf = pipeline("zero-shot-classification", model='cross-encoder/nli-MiniLM2-L6-H768')
+    candidate_labels = list(category_dict.values())
+    # Get model results
+    results = clf(x_test, candidate_labels)
+    predictions = []
+
+    for result in results:
+        label_pred = result['labels'][0]
+        predictions.append(candidate_labels.index(label_pred))
+
+    return accuracy_score(y_test, predictions)
+
+
+portions = [0.1, 0.5, 1.]
+
+logistic_acc = []
+print("Logistic regression results:")
+for p in portions:
+    print(f"Portion: {p}")
+    accuracy = linear_classification(p)
+    logistic_acc.append(accuracy)
+    print(accuracy)
+
+plt.figure()
+plt.plot(portions, logistic_acc, color="blue")
+plt.xlabel("portion of dataset")
+plt.ylabel("Logistice Regression accuracy")
+plt.title("Logistic Regression Accuracy as function of dataset portion")
+plt.show()
+
+fine_tune_acc = []
+print("\nFinetuning results:")
+for p in portions:
+    print(f"Portion: {p}")
+    accuracy = transformer_classification(portion=p)
+    print(accuracy)
+    fine_tune_acc.append(accuracy['eval_accuracy'])
+    print(accuracy['eval_accuracy'])
+
+for i in range(len(portions)):
+    print(f"Portion: {portions[i]}")
+    print(fine_tune_acc[i])
+
+plt.figure()
+plt.plot(portions, fine_tune_acc, color="blue")
+plt.xlabel("Portion of dataset")
+plt.ylabel("Accuracy")
+plt.title("Finetuning Model Accuracy as function of dataset portion")
+plt.show()
+
+
+print("\nZero-shot result:")
+print(zeroshot_classification())
